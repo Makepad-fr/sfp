1bef3a3b6f226df16ab372fa3589d3f272b77e59
Kaan Yagci <kaan.yagci@makepad.fr>
2024-05-01 19:12:41 +0200

    refactor: make hostname and port name come from environment variales
    Signed-off-by: Kaan Yagci <kaan.yagci@makepad.fr>

--- forward-proxy/main.go
+++ forward-proxy/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/Makepad-fr/sfp/core"
+import (
+	"os"
+
+	"github.com/Makepad-fr/sfp/core"
+)
+
+var hostname = ""
+var port = "9090"
+
+func init() {
+	val, ok := os.LookupEnv("SFP_HOSTNAME")
+	if ok {
+		hostname = val
+	}
+	val, ok = os.LookupEnv("SFP_PORT")
+	if ok {
+		port = val
+	}
+}
 
 func main() {
-	core.StartForwardProxy("localhost", "9090")
+	core.StartForwardProxy(hostname, port)
 }
